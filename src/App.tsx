d800ebe74a75cd3b400b90eaf0acbf3c608460e8
ernestoheres <ernestoheres09@gmail.com>
2025-11-19 11:56:50 +0100

    remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { use, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {  useState } from 'react'
 import { useEffect } from 'react'
 import './App.css'
 
